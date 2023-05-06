b4be2ab40c27b472b7cb8cfd5ab52b8caea31765
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-05-06 09:33:47 +0200

    Using fakeAsync and tick

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
 import { UserService } from './user.service';
@@ -68,5 +68,17 @@ describe('UserComponent', () => {
     fixture.whenStable().then(() => {
       expect(app.data).toBe('Data');
     });
+  }));
+
+  it('should fetch data successfully if not called asynchronously (fakeAsync)', fakeAsync(() => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    let dataService = fixture.debugElement.injector.get(DataService);
+    let spy = spyOn(dataService, 'getDetails').and.returnValue(
+      Promise.resolve('Data')
+    );
+    fixture.detectChanges();
+    tick();
+    expect(app.data).toBe('Data');
   }));
 });
